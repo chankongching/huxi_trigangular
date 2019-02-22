6b6128c5d7d6c1c1ad799933aed5a2e620a8e5a7
zihan <luixing-moon@163.com>
2019-02-22 14:11:33 +0800

    调整数据格式

--- ib/IB.py
+++ ib/IB.py
@@ -149,7 +149,7 @@ class IBClient(EWrapper):
             "bids": [[cache.get("bidPrice"), cache.get("bidSize")]],
             "symbol": product_name
         }
-        redis_client.set(product_name + "@" + PLATFORM.lower(), json.dumps(data), ex=120)
+        redis_client.set(product_name + "DEPTH@" + PLATFORM.lower(), json.dumps(data), ex=120)
         redis_client.publish(PLATFORM, json.dumps({"symbol": product_name, "time": time.time()}))
 
 
